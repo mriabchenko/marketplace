8374d967e2641ba3f5adda7b50eb59c89e0c382c
nikolay_r <nikolay5011995@gmail.com>
2017-07-02 18:55:09 +0300

    louped picture beta

--- src/components/global/picture_louped.vue
+++ src/components/global/picture_louped.vue
@@ -6,6 +6,7 @@
         @mousemove="handleMouse"
         @mouseenter="showLoupedArea = true"
         @mouseleave="showLoupedArea = false"
+        @wheel="handleWheel"
         class="picture_louped__origin"
         :src="image.src"
         :alt="image.alt">
@@ -16,7 +17,6 @@
       </div>
       <div
         class="picture_louped__louped"
-        v-if="showLoupedArea"
         title="image.alt"
         :style="loupedPictureStyle"
       ></div>
@@ -29,7 +29,12 @@
   	data(){
   		return {
   			showLoupedArea: false,
-        scale: 2,
+        lastMouseMoveEvent: undefined,
+        scale : {
+  				current: 2,
+          max: 10,
+          min: 2
+				},
   			origin: {
   				height: undefined,
           width: undefined,
@@ -58,28 +63,31 @@
     },
     methods:{
   		handleMouse(e){
+  			//saving event
+  			this.lastMouseMoveEvent = e;
         //defining origin size
 				this.origin.height = e.target.clientHeight;
 				this.origin.width = e.target.clientWidth;
 				//defining mouse position
 				this.origin.mx = e.offsetX;
 				this.origin.my = e.offsetY;
-				//defining loupedArea size using scale parameter
-				this.loupedAreaStyle.height = this.origin.height / this.scale + 'px';
-				this.loupedAreaStyle.width = this.origin.width / this.scale + 'px';
+				//defining loupedArea size using scale.current parameter
+				this.loupedAreaStyle.height = this.origin.height / this.scale.current + 'px';
+				this.loupedAreaStyle.width = this.origin.width / this.scale.current + 'px';
 				//defining loupedArea position
 				this.loupedAreaStyle.top = this.origin.my - parseInt(this.loupedAreaStyle.height) / 2 + 'px';
 				this.loupedAreaStyle.left = this.origin.mx - parseInt(this.loupedAreaStyle.width) / 2 + 'px';
 				//defining loupedPicture parameters
-				this.loupedPictureStyle.backgroundSize = parseInt(this.origin.width) * this.scale + 'px ' + parseInt(this.origin.height) * this.scale + 'px';
-				this.loupedPictureStyle.backgroundPositionX = ((-1) * this.scale * parseInt(this.loupedAreaStyle.top)) + 'px';
-				this.loupedPictureStyle.backgroundPositionY = ((-1) * this.scale * parseInt(this.loupedAreaStyle.left)) + 'px';
+				this.loupedPictureStyle.backgroundSize = parseInt(this.origin.width) * this.scale.current + 'px ' + parseInt(this.origin.height) * this.scale.current + 'px';
+				this.loupedPictureStyle.backgroundPositionY = ((-1) * this.scale.current * parseInt(this.loupedAreaStyle.top)) + 'px';
+				this.loupedPictureStyle.backgroundPositionX = ((-1) * this.scale.current * parseInt(this.loupedAreaStyle.left)) + 'px';
 
 				//console.log(this.origin.mx / this.origin.width,':',this.origin.my / this.origin.height);
       },
-      init(){
-
-      }
+      handleWheel(e){
+  			if (e.deltaY < 0) this.scale.current++
+        else if (e.deltaY > 0) this.scale.current--;
+      },
     },
     watch: {
 			//imposing restrictions on loupedArea position
@@ -94,10 +102,21 @@
 				if( parseInt(newValue) >= (this.origin.height - parseInt(this.loupedAreaStyle.height))) this.loupedAreaStyle.top = this.origin.height - parseInt(this.loupedAreaStyle.height) + 'px';
 			},
 			//loupedAreaBGposition
-			'loupedPictureStyle.backgroundPositionX'(){}
+      //x coordinate
+			'loupedPictureStyle.backgroundPositionX'(newValue){
+				if (parseInt(newValue) > 0) this.loupedPictureStyle.backgroundPositionX = 0;
+				if (parseInt(newValue) < ((-1) * parseInt(this.origin.width) * (this.scale.current - 1) )) this.loupedPictureStyle.backgroundPositionX = (-1) * parseInt(this.origin.width) * (this.scale.current - 1)  + 'px';
+      },
+			//y coordinate
+			'loupedPictureStyle.backgroundPositionY'(newValue){
+				if (parseInt(newValue) > 0) this.loupedPictureStyle.backgroundPositionY = 0;
+				if (parseInt(newValue) < ((-1) * parseInt(this.origin.height) * (this.scale.current - 1) )) this.loupedPictureStyle.backgroundPositionY = (-1) * parseInt(this.origin.height) * (this.scale.current - 1)  + 'px';
+			},
+      'scale.current'(newValue){
+				if (newValue < this.scale.min) this.scale.current = this.scale.min;
+				if (newValue > this.scale.max) this.scale.current = this.scale.max;
+				this.handleMouse(this.lastMouseMoveEvent);
+      }
     },
-    mounted(){
-  		this.init()
-    }
   }
 </script>
